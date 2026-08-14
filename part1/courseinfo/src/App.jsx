6e26ea14472824a47593f0fb8866f9b2c938850a
David Mimay <davidmimay@gmail.com>
2026-08-14 13:42:36 +0000

    1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,10 +9,17 @@ const Content = (props) => {
   console.log('Content is working')
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
+  )
+}
+
+const Part = (props) => {
+  console.log('Part is working')
+  return (
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
@@ -36,7 +43,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content
+      <Content 
         part1={part1} exercises1={exercises1}
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
