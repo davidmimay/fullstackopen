c36f7cdb378c16242f61fc46d1c355a07b785112
David Mimay <davidmimay@gmail.com>
2026-09-22 15:17:53 +0000

    2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,10 +1,48 @@
 import { useState, useEffect } from 'react'
 import countryService from './services/countries'
+import axios from 'axios'
 import './App.css'
+
 // Components
+const Weather = ({capital}) => {
+  const [weather, setWeather] = useState(null)
+  const apiKey = import.meta.env.VITE_WEATHER_KEY
+
+  console.log('API Key loaded:', apiKey)
+
+  useEffect(() => {
+    if (!capital) return
+
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${apiKey}`)
+    .then(response => {
+      setWeather(response.data)
+    })
+    .catch(error => {
+      console.log('Error fetching weather data', error)
+    })
+  }, [capital, apiKey])
+
+  if (!weather) {
+    return null
+  }
+
+  const iconCode = weather.weather[0].icon
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+
+  return (
+    <div className='weather'>
+      <h2>Weather in {capital}</h2>
+      <p>temperature {weather.main.temp} ºC</p>
+      <img src={iconUrl} alt={weather.weather[0].description} />
+      <p>wind {weather.wind.speed} m/s</p>
+    </div>
+  )
+}
 
 const CountryDetail = ({country}) => {
   const languages = Object.values(country.languages || {})
+  const capital = country.capital ? country.capital[0] : null
 
   return (
     <div>
@@ -21,6 +59,8 @@ const CountryDetail = ({country}) => {
         alt={`Flag of ${country.name.common}`}
         width="150"
       />
+
+      {capital && <Weather capital={capital}/>}
     </div>
   )
 }
